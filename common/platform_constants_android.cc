17319bc8875aae509df24f13bd775fba9afe0546
Dan Pasanen <dan.pasanen@gmail.com>
2017-03-15 09:09:06 -0500

    update_engine: don't mount /postinstall with context
    * We're going to need to be able to mess with contexts here via
      backuptools. Since we don't care about verity anyway, this isn't
      really a problem.
    
    Change-Id: I8ba3eea71c918904f5ec67bc5f57576895dd0ec5

--- common/platform_constants_android.cc
+++ common/platform_constants_android.cc
@@ -31,8 +31,7 @@ const char kCACertificatesPath[] = "/system/etc/security/cacerts_google";
 // No deadline file API support on Android.
 const char kOmahaResponseDeadlineFile[] = "";
 const char kNonVolatileDirectory[] = "/data/misc/update_engine";
-const char kPostinstallMountOptions[] =
-  "context=u:object_r:postinstall_file:s0";
+const char kPostinstallMountOptions[] = "";
 
 }  // namespace constants
 }  // namespace chromeos_update_engine
