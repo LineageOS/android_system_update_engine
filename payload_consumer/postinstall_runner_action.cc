11d92d5f7b85a3cb30c56c5e2dbea250313e3892
Alessandro Astone <ales.astone@gmail.com>
2021-03-21 11:46:41 +0100

    update_engine: Do not attempt to run backuptool in recovery
    Change-Id: I0d58eb87096a51cdaab9f66ab4f8bf257a3d2ff2

--- payload_consumer/postinstall_runner_action.cc
+++ payload_consumer/postinstall_runner_action.cc
@@ -179,7 +179,7 @@ bool PostinstallRunnerAction::MountPartition(
   EnsureUnmounted();
 
 #ifdef __ANDROID__
-#ifdef RUN_BACKUPTOOL
+#if !defined(__ANDROID_RECOVERY__) && defined(RUN_BACKUPTOOL)
   // Check the currently installed /system partition to see if it's ever
   // been mounted R/W. If it has, we'll run backuptool scripts for it
   // since we can safely assume something on the partition has been
@@ -246,7 +246,7 @@ bool PostinstallRunnerAction::MountPartition(
   }
 
   utils::UnmountFilesystem(fs_mount_dir_);
-#endif  // RUN_BACKUPTOOL
+#endif  // !__ANDROID_RECOVERY__ && RUN_BACKUPTOOL
 
   // In Chromium OS, the postinstall step is allowed to write to the block
   // device on the target image, so we don't mark it as read-only and should
