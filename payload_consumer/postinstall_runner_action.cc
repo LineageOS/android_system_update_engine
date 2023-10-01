470c4ef446b6bb9f62edcedb6566df59051e6ca0
Alexander Winkowski <dereference23@outlook.com>
2023-10-01 19:27:20 +0000

    Revert "update_engine: Run backuptool only in debuggable builds"
    This reverts commit d682a97ec4ccb9a869d3688fe93fc33bdf578b16.
    
    backuptool works on user builds now.
    
    Change-Id: I8456c14f170b7c6bd41c3869b0ef73ac2b15aca2

--- payload_consumer/postinstall_runner_action.cc
+++ payload_consumer/postinstall_runner_action.cc
@@ -179,7 +179,7 @@ bool PostinstallRunnerAction::MountPartition(
   EnsureUnmounted();
 
 #ifdef __ANDROID__
-#if !defined(__ANDROID_RECOVERY__) && defined(RUN_BACKUPTOOL)
+#if !defined(__ANDROID_RECOVERY__)
   // Check the currently installed /system partition to see if it's ever
   // been mounted R/W. If it has, we'll run backuptool scripts for it
   // since we can safely assume something on the partition has been
@@ -246,7 +246,7 @@ bool PostinstallRunnerAction::MountPartition(
   }
 
   utils::UnmountFilesystem(fs_mount_dir_);
-#endif  // !__ANDROID_RECOVERY__ && RUN_BACKUPTOOL
+#endif  // !__ANDROID_RECOVERY__
 
   // In Chromium OS, the postinstall step is allowed to write to the block
   // device on the target image, so we don't mark it as read-only and should
