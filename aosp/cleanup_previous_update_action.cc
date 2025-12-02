7bb6f0b9f3dedb514dcba4f65c8608ff01e0c044
Nolen Johnson <johnsonnolen@gmail.com>
2025-12-02 13:30:58 -0500

    Revert "Remove Virtual A/B retrofit support."
    This reverts commit 1cc8b74867d813175c7394b49904fc5e07168d8a.
    
    Reason: Several 4.14 and 4.19 shipping devices use this.
    Change-Id: Ic6498bb07626186130fcfc04497944f78145f9a9

--- aosp/cleanup_previous_update_action.cc
+++ aosp/cleanup_previous_update_action.cc
@@ -518,6 +518,9 @@ void CleanupPreviousUpdateAction::ReportMergeStats() {
   auto passed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(
       result->merge_time());
 
+  bool vab_retrofit = boot_control_->GetDynamicPartitionControl()
+                          ->GetVirtualAbFeatureFlag()
+                          .IsRetrofit();
   bool vab_compression_enabled = boot_control_->GetDynamicPartitionControl()
                                      ->GetVirtualAbCompressionFeatureFlag()
                                      .IsEnabled();
@@ -548,7 +551,7 @@ void CleanupPreviousUpdateAction::ReportMergeStats() {
                       static_cast<int32_t>(report.state()),
                       static_cast<int64_t>(passed_ms.count()),
                       static_cast<int32_t>(report.resume_count()),
-                      false, /* vab retrofit */
+                      vab_retrofit,
                       static_cast<int64_t>(report.cow_file_size()),
                       vab_compression_enabled,
                       vab_compression_used,
